280d7ab87d60127f9e1cc1b2bf13f2508236cf96
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-01 16:59:51 -0700

    using previously generated components investing them to the app components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe-detail',
+  selector: 'recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
