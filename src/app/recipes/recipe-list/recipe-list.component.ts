80869aa453a5a209f444a199a1526046b6ecaa83
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-04 16:12:58 -0700

    recipe-description

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [{
+    name: 'A new recipe',
+    description: 'test recipe',
+    imagePath: 'https://www.ndtv.com/cooks/images/iStock_50367092_SMALL%20%281%29.jpg'
+  }, new Recipe('a test recipe','this is a text','https://images.media-allrecipes.com/images/75131.jpg')];
 
   constructor() { }
 
