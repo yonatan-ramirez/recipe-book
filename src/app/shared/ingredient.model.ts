ad9a87a80eb7cc644038611ef6ddc7293ffe6233
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-04 16:50:09 -0700

    creates shopping list feature

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    
+    // constructor(name:string, amount: number){
+
+    // }
+
+    constructor(public name:string, public amount: number){
+
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[] = [
+  {
+    name: 'Apple',
+    amount: 5
+  }, 
+  new Ingredient('Tomtoes', 10)
+];
   constructor() { }
 
   ngOnInit() {
