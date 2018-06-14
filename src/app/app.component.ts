5da5cae25c5685a939b8ca3ce321886af25c591f
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-06-14 13:15:40 -0700

    add-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loadedFeature: string ='recipe';
+  navigate(feature:string){
+console.log('set new feature:', feature);
+this.loadedFeature ='featrue';
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'header-lmnt',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
+    @Output() featureSelected =new EventEmitter<string>()
+
+    selectFeature(feature:string){
+        this.featureSelected.emit(feature)
+    }
 
 }
