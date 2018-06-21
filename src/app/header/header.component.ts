466cb54017a49cafec18d2765a12e114d0f9540d
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-20 17:55:49 -0700

    nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'header-lmnt',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent{
+export class HeaderComponent {
 
 }
