e58c2f76bd4618a68a2a63ed187a47dda84d9c60
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-20 18:21:30 -0700

    small changes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from './../recipe.model';
+import { Component, OnInit, Input} from '@angular/core';
+import { Recipe } from 'src/app/recipes/recipe.model';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe:Recipe;
+  @Input() recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+
   selectedRecipe: Recipe;
   constructor() { }
 
   ngOnInit() {
   }
-  displayRecipe(recipe:Recipe){
+  displayRecipe(recipe: Recipe) {
     console.log('test');
 this.selectedRecipe = recipe;
   }
