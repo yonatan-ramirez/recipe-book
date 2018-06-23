e740a56250eec620a299ee5ccd09c6d1c1eb6c1a
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-22 17:41:12 -0700

    setting up services for our app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,17 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'header-lmnt',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent{
-    @Output() featureSelected =new EventEmitter<string>()
+export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
 
-    selectFeature(feature:string){
-        this.featureSelected.emit(feature)
+    selectFeature(feature: string) {
+        this.featureSelected.emit(feature);
     }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,24 +2,25 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- 
+
   recipes: Recipe[] = [{
     name: 'A new recipe',
     description: 'test recipe',
     imagePath: 'https://www.ndtv.com/cooks/images/iStock_50367092_SMALL%20%281%29.jpg'
-  }, new Recipe('a test recipe','this is a text','https://images.media-allrecipes.com/images/75131.jpg')];
+  }, new Recipe('a test recipe', 'this is a text', 'https://images.media-allrecipes.com/images/75131.jpg')];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
-  onRecipeSelected(recipe:Recipe){
+  onRecipeSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
   }
 
